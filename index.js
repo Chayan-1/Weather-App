c0427bc0b994f6c910aa55d766d22b0f213b09d6
Chayan <chayan24052003@gmail.com>
2024-07-18 23:35:25 +0530

    Added responsiveness

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ async function checkWeather(city){
         
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-        document.querySelector(".humidity").innerHTML = data.main.humidity + "% ";
-        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
         if(data.weather[0].main == "Clouds"){
             weatherIcon.src = "Images/clouds.png"
